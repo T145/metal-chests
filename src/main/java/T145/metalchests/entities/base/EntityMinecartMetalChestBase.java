fadcf5e6fe26707925cf4c69b656a64aeb656468
T145 <gnosoman@gmail.com>
2018-03-17 22:44:32 -0400

    Utilize AccessTransforming to use the MinecartChest's internal inventory

--- src/main/java/T145/metalchests/entities/base/EntityMinecartMetalChestBase.java
+++ src/main/java/T145/metalchests/entities/base/EntityMinecartMetalChestBase.java
@@ -14,21 +14,16 @@ import T145.metalchests.items.ItemChestStructureUpgrade;
 import T145.metalchests.lib.MetalChestType;
 import T145.metalchests.lib.MetalChestType.StructureUpgrade;
 import T145.metalchests.tiles.TileMetalChest;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityMinecartChest;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -36,15 +31,16 @@ import net.minecraftforge.event.entity.minecart.MinecartInteractEvent;
 
 public abstract class EntityMinecartMetalChestBase extends EntityMinecartChest {
 
-	private NonNullList<ItemStack> inventory = NonNullList.<ItemStack>withSize(getSizeInventory(), ItemStack.EMPTY);
 	private TileMetalChest chestInstance = new TileMetalChest(getChestType());
 
 	public EntityMinecartMetalChestBase(World world) {
 		super(world);
+		minecartContainerItems = NonNullList.<ItemStack>withSize(getSizeInventory(), ItemStack.EMPTY);
 	}
 
 	public EntityMinecartMetalChestBase(World world, double x, double y, double z) {
 		super(world, x, y, z);
+		minecartContainerItems = NonNullList.<ItemStack>withSize(getSizeInventory(), ItemStack.EMPTY);
 	}
 
 	public static EntityMinecartMetalChestBase create(World world, double x, double y, double z, MetalChestType type) {
@@ -73,7 +69,7 @@ public abstract class EntityMinecartMetalChestBase extends EntityMinecartChest {
 	}
 
 	public void updateChestInstance() {
-		chestInstance.setInventory(inventory);
+		chestInstance.setInventory(minecartContainerItems);
 		chestInstance.sortTopStacks();
 	}
 
@@ -133,14 +129,12 @@ public abstract class EntityMinecartMetalChestBase extends EntityMinecartChest {
 					setDead();
 
 					world.spawnEntity(newCart);
-					newCart.updateChestInstance();
 
 					if (!player.capabilities.isCreativeMode) {
 						stack.shrink(1);
 					}
 
 					player.world.playSound(null, player.getPosition(), SoundEvents.BLOCK_ANVIL_PLACE, SoundCategory.PLAYERS, 0.4F, 0.8F);
-
 					return true;
 				}
 			}
@@ -154,85 +148,5 @@ public abstract class EntityMinecartMetalChestBase extends EntityMinecartChest {
 	@Override
 	public IBlockState getDefaultDisplayTile() {
 		return ModLoader.METAL_CHEST.getDefaultState().withProperty(BlockMetalChest.VARIANT, getChestType());
-	}
-
-	@Override
-	public ItemStack getStackInSlot(int slot) {
-		addLoot(null);
-		return inventory.get(slot);
-	}
-
-	@Override
-	public ItemStack decrStackSize(int slot, int size) {
-		addLoot(null);
-		return ItemStackHelper.getAndSplit(inventory, slot, size);
-	}
-
-	@Override
-	public ItemStack removeStackFromSlot(int index) {
-		addLoot(null);
-		ItemStack itemstack = inventory.get(index);
-
-		if (itemstack.isEmpty()) {
-			return ItemStack.EMPTY;
-		} else {
-			inventory.set(index, ItemStack.EMPTY);
-			return itemstack;
-		}
-	}
-
-	@Override
-	public void setInventorySlotContents(int index, ItemStack stack) {
-		addLoot(null);
-		inventory.set(index, stack);
-
-		if (!stack.isEmpty() && stack.getCount() > getInventoryStackLimit()) {
-			stack.setCount(getInventoryStackLimit());
-		}
-	}
-
-	@Override
-	protected void writeEntityToNBT(NBTTagCompound compound) {
-		if (hasDisplayTile()) {
-			compound.setBoolean("CustomDisplayTile", true);
-			IBlockState iblockstate = getDisplayTile();
-			ResourceLocation resourcelocation = Block.REGISTRY.getNameForObject(iblockstate.getBlock());
-			compound.setString("DisplayTile", resourcelocation == null ? "" : resourcelocation.toString());
-			compound.setInteger("DisplayData", iblockstate.getBlock().getMetaFromState(iblockstate));
-			compound.setInteger("DisplayOffset", getDisplayTileOffset());
-		}
-
-		if (getLootTable() != null) {
-			compound.setString("LootTable", getLootTable().toString());
-
-			// need accesstransformer for the seed (if we ever want to add in dungeon gen)
-		} else {
-			ItemStackHelper.saveAllItems(compound, inventory);
-		}
-	}
-
-	@Override
-	protected void readEntityFromNBT(NBTTagCompound compound) {
-		if (compound.getBoolean("CustomDisplayTile")) {
-			Block block;
-
-			if (compound.hasKey("DisplayTile", 8)) {
-				block = Block.getBlockFromName(compound.getString("DisplayTile"));
-			} else {
-				block = Block.getBlockById(compound.getInteger("DisplayTile"));
-			}
-
-			int i = compound.getInteger("DisplayData");
-			setDisplayTile(block == null ? Blocks.AIR.getDefaultState() : block.getStateFromMeta(i));
-			setDisplayTileOffset(compound.getInteger("DisplayOffset"));
-		}
-
-		inventory = NonNullList.<ItemStack>withSize(getSizeInventory(), ItemStack.EMPTY);
-
-		if (compound.hasKey("LootTable", 8)) {
-			setLootTable(new ResourceLocation(compound.getString("LootTable")), compound.getLong("LootTableSeed"));
-		} else {
-			ItemStackHelper.loadAllItems(compound, inventory);
-		}
 	}
 }
