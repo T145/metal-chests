5cbef7bb661049826b6db6a0f26d96ded9cf178d
T145 <T145@protonmail.com>
2018-08-12 01:07:10 -0400

    Remove block event triggers for hungry metal chests
    Doesn't cause the "nom" animation anymore

--- src/main/java/T145/metalchests/compat/thaumcraft/TileHungryMetalChest.java
+++ src/main/java/T145/metalchests/compat/thaumcraft/TileHungryMetalChest.java
@@ -27,17 +27,4 @@ public class TileHungryMetalChest extends TileMetalChest {
 	public TileHungryMetalChest() {
 		super();
 	}
-
-	@Override
-	public boolean receiveClientEvent(int id, int data) {
-		switch (id) {
-		case 2:
-			if (lidAngle < data / 10F) {
-				lidAngle = data / 10F;
-			}
-			return true;
-		default:
-			return super.receiveClientEvent(id, data);
-		}
-	}
 }
