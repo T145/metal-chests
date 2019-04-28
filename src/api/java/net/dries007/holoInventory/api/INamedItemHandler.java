772c8838fb2f6665d095f7cb3fa5dc5f372c2a16
T145 <T145@protonmail.com>
2019-04-27 20:17:47 -0400

    If Quark is installed, be able to right-click chests into minecarts
    Resolves #58 & #62
    
    Though the latter is already resolved

--- src/api/java/net/dries007/holoInventory/api/INamedItemHandler.java
+++
@@ -1,9 +0,0 @@
-package net.dries007.holoInventory.api;
-
-public interface INamedItemHandler {
-
-	/*
-	 * Option for other mods using IItemHandler to have their names properly detected
-	 */
-	String getItemHandlerName();
-}
