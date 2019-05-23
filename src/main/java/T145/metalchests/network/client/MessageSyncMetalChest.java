1e7bb43fb316f50d0a969cad8c8a3894f05b7787
T145 <T145@protonmail.com>
2019-05-23 10:36:30 -0400

    Cleaned up packet

--- src/main/java/T145/metalchests/network/client/MessageSyncMetalChest.java
+++ src/main/java/T145/metalchests/network/client/MessageSyncMetalChest.java
@@ -3,12 +3,10 @@ package T145.metalchests.network.client;
 import java.io.IOException;
 
 import T145.metalchests.api.chests.IMetalChest;
-import T145.metalchests.api.constants.ChestType;
 import T145.metalchests.network.MessageBase;
 import T145.metalchests.tiles.TileMetalChest;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -16,8 +14,6 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 public class MessageSyncMetalChest extends MessageBase {
 
 	protected BlockPos pos;
-	protected ChestType type;
-	protected EnumFacing front;
 	protected boolean trapped;
 	protected boolean luminous;
 	protected byte enchantLevel;
@@ -27,8 +23,6 @@ public class MessageSyncMetalChest extends MessageBase {
 	}
 
 	protected void setChestData(IMetalChest chest) {
-		this.type = chest.getChestType();
-		this.front = chest.getFront();
 		this.trapped = chest.isTrapped();
 		this.luminous = chest.isLuminous();
 		this.enchantLevel = chest.getEnchantLevel();
@@ -42,8 +36,6 @@ public class MessageSyncMetalChest extends MessageBase {
 	@Override
 	public void serialize(PacketBuffer buf) {
 		buf.writeBlockPos(pos);
-		buf.writeEnumValue(type);
-		buf.writeEnumValue(front);
 		buf.writeBoolean(trapped);
 		buf.writeBoolean(luminous);
 		buf.writeByte(enchantLevel);
@@ -52,16 +44,12 @@ public class MessageSyncMetalChest extends MessageBase {
 	@Override
 	public void deserialize(PacketBuffer buf) throws IOException {
 		pos = buf.readBlockPos();
-		type = buf.readEnumValue(ChestType.class);
-		front = buf.readEnumValue(EnumFacing.class);
 		trapped = buf.readBoolean();
 		luminous = buf.readBoolean();
 		enchantLevel = buf.readByte();
 	}
 
 	protected void syncChestData(IMetalChest chest) {
-		chest.setChestType(type);
-		chest.setFront(front);
 		chest.setTrapped(trapped);
 		chest.setLuminous(luminous);
 		chest.setEnchantLevel(enchantLevel);
