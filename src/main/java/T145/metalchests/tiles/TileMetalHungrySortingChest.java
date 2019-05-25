ab46ae890779c1849e6324ac2d3715498ebbaff9
T145 <T145@protonmail.com>
2019-05-25 12:55:37 -0400

    Updated license headers

--- src/main/java/T145/metalchests/tiles/TileMetalHungrySortingChest.java
+++ src/main/java/T145/metalchests/tiles/TileMetalHungrySortingChest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2019 T145
+ * Copyright 2018-2019 T145
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
