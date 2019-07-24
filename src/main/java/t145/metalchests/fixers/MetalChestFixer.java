7c3e414631d5c26951f8ef21f24532ee8639c9ab
T145 <T145@protonmail.com>
2019-07-24 15:46:42 -0400

    Fixed the fixer

--- src/main/java/t145/metalchests/fixers/MetalChestFixer.java
+++ src/main/java/t145/metalchests/fixers/MetalChestFixer.java
@@ -1,15 +1,13 @@
 package t145.metalchests.fixers;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityList;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.datafix.DataFixesManager;
 import net.minecraft.util.datafix.IDataFixer;
 import net.minecraft.util.datafix.IDataWalker;
 import t145.metalchests.api.consts.RegistryMC;
+import t145.metalchests.tiles.TileMetalChest;
 
 public class MetalChestFixer implements IDataWalker {
 
@@ -21,36 +19,30 @@ public class MetalChestFixer implements IDataWalker {
 		IDS.put(RegistryMC.getResource("sorting_hungry_metal_chest"), RegistryMC.RESOURCE_METAL_SORTING_HUNGRY_CHEST);
 	}
 
-	private final ResourceLocation key;
+	private final Class<? extends TileMetalChest> chestClass;
 
-	public MetalChestFixer(Class<?> entityClass) {
-		if (Entity.class.isAssignableFrom(entityClass)) {
-			this.key = EntityList.getKey((Class<Entity>) entityClass);
-		} else if (TileEntity.class.isAssignableFrom(entityClass)) {
-			this.key = TileEntity.getKey((Class<TileEntity>) entityClass);
+	public MetalChestFixer(Class<? extends TileMetalChest> chestClass) {
+		if (TileMetalChest.class.isAssignableFrom(chestClass)) {
+			this.chestClass = chestClass;
 		} else {
-			this.key = null;
+			this.chestClass = null;
 		}
 	}
 
 	@Override
 	public NBTTagCompound process(IDataFixer fixer, NBTTagCompound tag, int version) {
-		if ((new ResourceLocation(tag.getString("id"))).equals(this.key)) {
-			tag = this.filteredProcess(fixer, tag, version);
-		}
+		RegistryMC.LOG.info(chestClass);
+		if (chestClass != null) {
+			RegistryMC.LOG.info("WE BE FIXING THINGS!");
+			String id = tag.getString("id");
 
-		return tag;
-	}
+			tag.setString("id", IDS.getOrDefault(id, new ResourceLocation(id)).toString());
 
-	public NBTTagCompound filteredProcess(IDataFixer fixer, NBTTagCompound tag, int version) {
-		tag = DataFixesManager.processInventory(fixer, tag, version, "Items");
+			if (tag.hasKey("Luminous")) {
+				tag.removeTag("Luminous");
+			}
 
-		String id = tag.getString("id");
-
-		tag.setString("id", IDS.getOrDefault(id, new ResourceLocation(id)).toString());
-
-		if (tag.hasKey("Luminous")) {
-			tag.removeTag("Luminous");
+			tag = DataFixesManager.processInventory(fixer, tag, version, "Items");
 		}
 
 		return tag;
