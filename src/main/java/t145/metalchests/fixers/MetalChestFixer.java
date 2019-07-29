760682e07abb741d96518f6af59db51b53675c62
T145 <T145@protonmail.com>
2019-07-29 14:09:12 -0400

    Cleaned up the chest fixer

--- src/main/java/t145/metalchests/fixers/MetalChestFixer.java
+++ src/main/java/t145/metalchests/fixers/MetalChestFixer.java
@@ -1,6 +1,8 @@
 package t145.metalchests.fixers;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
@@ -13,40 +15,42 @@ import t145.metalchests.tiles.TileMetalChest;
 
 public class MetalChestFixer implements IDataWalker {
 
-	private static final Object2ObjectOpenHashMap<ResourceLocation, ResourceLocation> IDS = new Object2ObjectOpenHashMap<>();
+	private static final Map<String, String> IDS = new HashMap<>();
 
 	static {
-		IDS.put(RegistryMC.getResource("hungry_metal_chest"), RegistryMC.RESOURCE_METAL_HUNGRY_CHEST);
-		IDS.put(RegistryMC.getResource("sorting_metal_chest"), RegistryMC.RESOURCE_METAL_SORTING_CHEST);
-		IDS.put(RegistryMC.getResource("sorting_hungry_metal_chest"), RegistryMC.RESOURCE_METAL_SORTING_HUNGRY_CHEST);
+		IDS.put("metalchests:hungry_metal_chest", RegistryMC.RESOURCE_METAL_HUNGRY_CHEST.toString());
+		IDS.put("metalchests:sorting_metal_chest", RegistryMC.RESOURCE_METAL_SORTING_CHEST.toString());
+		IDS.put("metalchests:sorting_hungry_metal_chest", RegistryMC.RESOURCE_METAL_SORTING_HUNGRY_CHEST.toString());
 	}
 
 	private final Class<? extends TileMetalChest> chestClass;
 
 	public MetalChestFixer(Class<? extends TileMetalChest> chestClass) {
-		if (TileMetalChest.class.isAssignableFrom(chestClass)) {
-			this.chestClass = chestClass;
-		} else {
-			this.chestClass = null;
-		}
+		this.chestClass = TileMetalChest.class.isAssignableFrom(chestClass) ? chestClass : null;
 	}
 
 	@Override
 	public NBTTagCompound process(IDataFixer fixer, NBTTagCompound tag, int version) {
 		if (chestClass != null) {
 			String id = tag.getString("id");
+			ResourceLocation res = new ResourceLocation(id);
 
-			tag.setString("id", IDS.getOrDefault(id, new ResourceLocation(id)).toString());
+			if (res.getNamespace().contentEquals(RegistryMC.ID)) {
+				RegistryMC.LOG.info("WE'RE FIXING THINGS!!!");
+				String path = res.toString();
 
-			if (tag.hasKey("Luminous")) {
-				tag.removeTag("Luminous");
-			}
+				tag.setString("id", IDS.getOrDefault(path, path));
 
-			if (tag.hasKey(IMetalChest.TAG_INVENTORY)) {
-				NBTTagList itemTags = tag.getCompoundTag(IMetalChest.TAG_INVENTORY).getTagList("Items", 10);
+				if (tag.hasKey("Luminous")) {
+					tag.removeTag("Luminous");
+				}
 
-				for (short i = 0; i < itemTags.tagCount(); ++i) {
-					itemTags.set(i, fixer.process(FixTypes.ITEM_INSTANCE, itemTags.getCompoundTagAt(i), version));
+				if (tag.hasKey(IMetalChest.TAG_INVENTORY)) {
+					NBTTagList itemTags = tag.getCompoundTag(IMetalChest.TAG_INVENTORY).getTagList("Items", 10);
+
+					for (short i = 0; i < itemTags.tagCount(); ++i) {
+						itemTags.set(i, fixer.process(FixTypes.ITEM_INSTANCE, itemTags.getCompoundTagAt(i), version));
+					}
 				}
 			}
 		}
